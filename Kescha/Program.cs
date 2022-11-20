0331501543abfa835e3e2887d60bb8a1f80a5f6f
zafarxayrullayev <xayrullayevzafar49@gamil.com>
2022-11-20 06:48:48 +0500

    added the introduction part

---
+++ Kescha/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kescha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name : ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your first name : ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter your age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Hello {name} {firstName}, Sizning yoshingiz {age} da ekan");
+        }
+    }
+}
