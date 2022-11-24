e80cc06d89314b27bd19687a9925fcf78aeedf88
zafarxayrullayev <xayrullayevzafar49@gmail.com>
2022-11-24 10:56:15 +0500

    created construktor and worked with classes, objects

--- ClassLibrary1/Animal.cs
+++ ClassLibrary1/Animal.cs
@@ -9,6 +9,19 @@ namespace ClassLibrary1
     public class Animal
     {
         public int Age { get; set; }
+        public int AgeDifference { get; set; }
+        public int AnimalAge { get; set; }
+        public string Name { get; set; }
+
+        public Animal(int userAge) 
+        {
+            Console.WriteLine("Enter animal name!");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter animal age!");
+            Age = Convert.ToInt32(Console.ReadLine());
+            AgeDifference = Math.Abs(userAge - Age);
+             
+        } 
 
     }
 }
--- Kescha/Program.cs
+++ Kescha/Program.cs
@@ -16,7 +16,7 @@ namespace Kescha
 
             do
             {
-                Console.WriteLine("Parolni kitit : ");
+                Console.Write("Parolni kitit : ");
                 password = Console.ReadLine();
             } while (password != "mykescha");
             Console.Write("Enter your name : ");
@@ -26,41 +26,40 @@ namespace Kescha
             Console.Write("Enter your age : ");
             int age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Hello {name} {firstName}, Sizning yoshingiz {age} da ekan");
-            Console.WriteLine("Sizning Keschangizning yoshi nechchida? yoki nechinchi yil tug'lgan?");
-            Console.WriteLine("Yosh kiritsangiz 1 ni, yil kiritsangiz 2 ni kiriting!");
-            int tanla = Convert.ToInt32(Console.ReadLine());
 
-            Animal kescha = new Animal();
-            int keschaBorn, nowYear;
-            switch (tanla)
-            {
-                case 1:
-                    Console.Write("Yoshni tanladingiz, yoshni kiriting! ");
-                    kescha.Age = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine($"Yosh farqi {Math.Abs(age - kescha.Age)} ga teng.");
-                    break;
-                case 2:
-                    Console.Write("Yilni tanladingiz, yilini kiriting! ");
-                z:
-                    keschaBorn = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Hozirgi yilni kiriting! ");
-                    nowYear = Convert.ToInt32(Console.ReadLine());
-                    if (nowYear < keschaBorn)
-                    {
-                        Console.WriteLine("Hozirgi yil tug'ilgan yildan yo katta yo teng bo'ladi!!!\nQayta kiriting : ");
-                        goto z;
-                    }
-                    Console.WriteLine($"Yosh farqi {Math.Abs(age - (nowYear - keschaBorn))}");
-                    break;
-                default:
-                    break;
-            }
+            Animal kescha = new Animal(userAge: age);
+            Console.WriteLine($"Sizning {kescha.Name} bilan yosh farqingiz {kescha.AgeDifference} ga teng");
+            //int tanla = Convert.ToInt32(Console.ReadLine());
+            //int keschaBorn, nowYear;
+            //switch (tanla)
+            //{
+            //    case 1:
+            //        Console.Write("Yoshni tanladingiz, yoshni kiriting! ");
+            //        kescha.Age = Convert.ToInt32(Console.ReadLine());
+            //        Console.WriteLine($"Yosh farqi {Math.Abs(age - kescha.Age)} ga teng.");
+            //        break;
+            //    case 2:
+            //        Console.Write("Yilni tanladingiz, yilini kiriting! ");
+            //    z:
+            //        keschaBorn = Convert.ToInt32(Console.ReadLine());
+            //        Console.Write("Hozirgi yilni kiriting! ");
+            //        nowYear = Convert.ToInt32(Console.ReadLine());
+            //        if (nowYear < keschaBorn)
+            //        {
+            //            Console.WriteLine("Hozirgi yil tug'ilgan yildan yo katta yo teng bo'ladi!!!\nQayta kiriting : ");
+            //            goto z;
+            //        }
+            //        Console.WriteLine($"Yosh farqi {Math.Abs(age - (nowYear - keschaBorn))}");
+            //        break;
+            //    default:
+            //        break;
+            //}
 
             
 
             if (age < kescha.Age)
             {
-                Console.WriteLine("You are younger than Kescha");
+                Console.WriteLine($"You are younger than {kescha.Name}");
             }
             else if (age == kescha.Age)
             {
@@ -68,20 +67,20 @@ namespace Kescha
             }
             else
             {
-                Console.WriteLine("You are older than Kescha");
+                Console.WriteLine($"You are older than {kescha.Name}");
             }
 
-            string[] friendsName = { "Ali", "Vali", "Aziz" };
-            int[] friendsAge = { 10, 18, 12 };
-            string[] adress = { "sam", "tosh", "buh" };
-            //            Console.WriteLine($"{friendsName[0]} is {friendsAge[0]} years old");
-            //            Console.WriteLine($"{friendsName[1]} is {friendsAge[1]} years old");
-            //            Console.WriteLine($"{friendsName[0]} was born in {adress[0]}");
+            //string[] friendsName = { "Ali", "Vali", "Aziz" };
+            //int[] friendsAge = { 10, 18, 12 };
+            //string[] adress = { "sam", "tosh", "buh" };
+            //Console.WriteLine($"{friendsName[0]} is {friendsAge[0]} years old");
+            //Console.WriteLine($"{friendsName[1]} is {friendsAge[1]} years old");
+            //Console.WriteLine($"{friendsName[0]} was born in {adress[0]}");
 
-            foreach (string names in friendsName)
-            {
-                Console.WriteLine(names);
-            }
+            //foreach (string names in friendsName)
+            //{
+            //    Console.WriteLine(names);
+            //}
         }
     }
 }
